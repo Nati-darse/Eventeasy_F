eb3645138074a13d56df5c88aa2e3bb3bb956e8f
Natnael Darsema <161618344+Nati-darse@users.noreply.github.com>
2025-06-22 00:51:22 +0300

    Fix case sensitivity in NavBar import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import NavBar from './Components/navBar';  
+import NavBar from './components/navBar';  
 import HomePage from "./Pages/HomePage";
 import { Route, Routes } from 'react-router-dom';
 import Classify_SU from './Pages/Classify_SU';
